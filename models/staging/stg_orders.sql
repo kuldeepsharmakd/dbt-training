cb5054f250afad683a65f576d506cf4a7ecc796e
kuldeep sharma <kuldeepsharma.kd@gmail.com>
2023-11-04 15:44:06 +0000

    new model stg_orders development

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,23 @@
+select 
+-- from raw order
+o.ORDERID,
+o.ORDERDATE,
+o.SHIPDATE,
+o.SHIPMODE,
+o.ORDERSELLINGPRICE - o.ORDERCOSTPRICE as order_profit,
+o.ORDERSELLINGPRICE,
+o.ORDERCOSTPRICE,
+-- from raw customer
+c.CUSTOMERNAME,
+c.SEGMENT,
+c.COUNTRY,
+-- from raw product
+p.CATEGORY,
+p.PRODUCTNAME,
+p.SUBCATEGORY
+from
+{{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.CUSTOMERID = c.CUSTOMERID
+left join {{ ref('raw_product') }} as p
+on o.productid = p.PRODUCTID
